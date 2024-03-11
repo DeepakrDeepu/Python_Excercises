efdd167aee7fdc1bb32b477bc9121a08030b02fa
DeepakrDeepu <162397515+DeepakrDeepu@users.noreply.github.com>
2024-03-11 19:34:28 +0530

    Multiplication Table using while loop

---
+++ Multiplication_Table_Using_While_Loop.py
@@ -0,0 +1,9 @@
+num = (int(input("Enter the multiplication value in number= ")))
+iterations = 1
+while iterations <= 10:
+    mul = num * iterations
+    print(num, '*', iterations, '=', mul)
+    iterations += 1
+    
+
+
