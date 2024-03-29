270a8583a8abf675fd126fa9f4864168e325fc96
DeepakrDeepu <162397515+DeepakrDeepu@users.noreply.github.com>
2024-03-30 00:39:58 +0530

    Add files via upload

---
+++ Stops_Refuelling.py
@@ -0,0 +1,8 @@
+Distance_Travelled = 560
+Car_Mileage = 10
+Tank_Capacity = 30
+Total_Fuel_Needed = Distance_Travelled / Car_Mileage
+Number_Stops = Total_Fuel_Needed / Tank_Capacity
+Number_Stops = int(Number_Stops) + 1
+print ("Number of stops taken during the travell from bangalore to goa: ", Number_Stops)
+
