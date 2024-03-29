99d4ee0643ff9811b70de58857071e4e65b3f082
DeepakrDeepu <162397515+DeepakrDeepu@users.noreply.github.com>
2024-03-30 00:48:31 +0530

    Update Stops_Refuelling.py
    Have included for user inputs

--- Stops_Refuelling.py
+++ Stops_Refuelling.py
@@ -1,8 +1,7 @@
 Distance_Travelled = 560
-Car_Mileage = 10
-Tank_Capacity = 30
+Car_Mileage = float(input("Enter the car Mileage: "))
+Tank_Capacity = float(input("Enter the tank capacity of the car: "))
 Total_Fuel_Needed = Distance_Travelled / Car_Mileage
 Number_Stops = Total_Fuel_Needed / Tank_Capacity
 Number_Stops = int(Number_Stops) + 1
 print ("Number of stops taken during the travell from bangalore to goa: ", Number_Stops)
-
