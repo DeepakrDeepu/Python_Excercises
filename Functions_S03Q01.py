6e1481ebffee7e2285a06cf47c6960e1e200c58f
DeepakrDeepu <162397515+DeepakrDeepu@users.noreply.github.com>
2024-04-04 01:06:19 +0530

    Functions_S03Q01

---
+++ Functions_S03Q01.py
@@ -0,0 +1,15 @@
+num1 = int(input("Enter the first number: "))
+if 10 <= num1 <= 99:
+    print("Entered number is 2-digit number")
+elif 100 <= num1 <= 999:
+    print("Entered number is 3-digit number")
+else:
+    print(num1)
+    
+num2 = int(input("Enter the second number: "))
+if 10 <= num2 <= 99:
+    print("Entered number is 2-digit number")
+elif 100 <= num2 <= 999:
+    print("Entered number is 3-digit number")
+else:
+    print(num2)
